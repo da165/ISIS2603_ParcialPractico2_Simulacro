41468e249e39b21b70903fb6708976fd366b5e9e
David Ortiz Alfaro <da.ortiza1@uniandes.edu.co>
2025-11-19 00:12:06 -0500

    avances #2

--- src/app/recipe/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Recipe } from '../Recipe';
+import { RecipeService } from '../../recipe.service';
+
+
 
 @Component({
   selector: 'app-recipe-detail',
@@ -8,7 +13,22 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: any;
+  private isRouteMode: boolean = false;
 
-  constructor() {}
-  ngOnInit(): void {} 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private recipeService: RecipeService
+  ) {}
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.isRouteMode = true;
+      this.cargarRecipe(+id);
+    }
+  } 
+  cargarRecipe(id: number) {
+    this.recipeService.getRecipeDetails(id).subscribe((data) => {
+      this.recipe = data;},(error)=>{ console.error("Error al cargar: ", error);});    
+  }
 }
--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../Recipe';
 import { recipeData } from '../recipeData';
+import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -13,14 +14,17 @@ export class RecipeListComponent implements OnInit {
   selected: Boolean = false;
   selectedRecipe: Recipe | null = null;
 
-  constructor() {}
+  constructor(private recipeService: RecipeService) {}
 
   ngOnInit() {
     this.recipes = recipeData;
+    this.recipeService.getRecipes().subscribe((recipeData)=>{
+      this.recipes = recipeData;
+    });
   }
-
   onSelect(recipe: Recipe) {
     this.selectedRecipe = recipe;
     this.selected = true;
   }
+  
 }
